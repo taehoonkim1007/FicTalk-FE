068e847322298fff025ac157648873ba7e1c1725
taehoonkim1007 <dsst1007@gmail.com>
2026-01-28 17:45:37 +0900

    feat: 신규 페이지 라우팅 등록 - 검색, 캐릭터 상세, 캐릭터 목록 페이지 라우트 추가 - 전체 라우팅 구조 최신화

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,6 +5,9 @@ import { MainLayout } from "@/components/layout/MainLayout";
 import { AuthCallbackPage } from "@/pages/AuthCallbackPage";
 import { HomePage } from "@/pages/HomePage";
 import { LoginPage } from "@/pages/LoginPage";
+import { SearchPage } from "@/pages/SearchPage";
+import { CharacterDetailPage } from "@/pages/character/CharacterDetailPage";
+import { CharactersPage } from "@/pages/character/CharactersPage";
 import { MyStoriesPage } from "@/pages/story/MyStoriesPage";
 import { StoriesPage } from "@/pages/story/StoriesPage";
 import { StoryDetailPage } from "@/pages/story/StoryDetailPage";
@@ -20,6 +23,10 @@ export const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
+        path: "search",
+        element: <SearchPage />,
+      },
+      {
         path: "stories/:storyId",
         element: <StoryDetailPage />,
       },
@@ -30,19 +37,33 @@ export const router = createBrowserRouter([
             path: "my-stories",
             element: <MyStoriesPage />,
           },
-          {
-            path: "stories/new",
-            element: <StoryFormPage />,
-          },
-          {
-            path: "stories/:storyId/edit",
-            element: <StoryFormPage />,
-          },
         ],
+      },
+      {
+        path: "characters/:characterId",
+        element: <CharacterDetailPage />,
+      },
+      {
+        path: ":categorySlug/characters",
+        element: <CharactersPage />,
       },
       {
         path: ":categorySlug",
         element: <StoriesPage />,
+      },
+    ],
+  },
+  // 스토리 작성/수정 페이지 (MainLayout 밖)
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/stories/new",
+        element: <StoryFormPage />,
+      },
+      {
+        path: "/stories/:storyId/edit",
+        element: <StoryFormPage />,
       },
     ],
   },
